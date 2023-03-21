6d72246dd2c7271f6b497443e35f3c949ee4adf4
Lethargic Logistics <61340443+LogisticsAndLaziness@users.noreply.github.com>
2023-03-21 14:36:57 -0400

    Update Character.java
    Updates before merge / i'm tired and might just make this the master

--- Character.java
+++ Character.java
@@ -3,12 +3,26 @@ import java.util.ArrayList;
 
 public class Character {
     ArrayList<Ability> abilitySet;
-    String region;
+    String region, diff;
     Ability a,b,c,d;
-    public Character(String region, Ability a, Ability b, Ability c, Ability d){
+    double health;
+    double hard = 2.5;
+    double medium = 1.5;
+    double easy = 0.5;
+    public Character(String region, String diff, Ability a, Ability b, Ability c, Ability d, double health){
         this.region = region;
         abilitySet = new ArrayList<Ability>(4);  
-        setAbilities(a,b,c,d);     
+        setAbilities(a,b,c,d);  
+
+        if(diff.equals("easy")){
+            this.health = health*easy;
+        }
+        else if(diff.equals("hard")){
+            this.health = health*hard;
+        }
+        else if(diff.equals("medium")){
+            this.health = health;
+        }
     }
     public void setAbilities(Ability a, Ability b, Ability c, Ability d){
         addAbility(a);
@@ -24,5 +38,11 @@ public class Character {
     }
     public Ability getAbility(int i){
         return abilitySet.get(i);
+    }
+    public double getHealth(){
+        return health;
+    }
+    public void setHealth(double b){
+        this.health = b;
     }
 }
