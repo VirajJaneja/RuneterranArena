ff920a39e0f682e525cf16111c604120ee9f4e67
VirajJaneja <virajjaneja@gmail.com>
2023-03-29 09:32:05 -0400

    did board stuff, wip

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,28 @@
 
 public class Board {
-    Character p1DeployedCharacter;
+    public Player playerOne;
+    public Player playerTwo;
+
+    public Character[] teamOne;
+    public Character[] teamTwo;
+
+    public Character deployedCharacOne;
+    public Character deployedCharacTwo;
+
+    public Board(Player playerUno, Player playerDos){
+        playerOne = playerUno;
+        playerTwo = playerDos;
+    }
+
+    public void dealDamage(Character target, double damage){
+        deployedCharacOne.updateHealth(deployedCharacOne.getHealth()-double);
+    }
+
+    public void
+
+
+
+
+
 }
+
