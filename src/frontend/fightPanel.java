7ddc0901c62992414bf8df7450706ed529f91f08
Lethargic Logistics <61340443+LogisticsAndLaziness@users.noreply.github.com>
2023-05-15 02:41:10 -0400

    smal
    chan

--- src/frontend/fightPanel.java
+++ src/frontend/fightPanel.java
@@ -2,5 +2,16 @@ package frontend;
 import javax.swing.*;
 
 public class fightPanel extends JPanel{
-    
+    private JLabel player;
+    private JLabel computer;
+
+    public fightPanel(){
+
+        player = new JLabel();
+        player.setIcon(new javax.swing.ImageIcon("lib/GarenGreenPlayer.gif"));
+        add(player);
+    }
+
+    public static void main(String[] args){
+    }
 }
