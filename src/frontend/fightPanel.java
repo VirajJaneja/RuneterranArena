7c3e18c920e29267ca2c2d50d2fa4a831599e654
Lethargic Logistics <61340443+LogisticsAndLaziness@users.noreply.github.com>
2023-05-15 02:51:26 -0400

    comp garen

--- src/frontend/fightPanel.java
+++ src/frontend/fightPanel.java
@@ -1,15 +1,24 @@
 package frontend;
+import java.awt.GridLayout;
+
 import javax.swing.*;
 
 public class fightPanel extends JPanel{
     private JLabel player;
     private JLabel computer;
+    private GridLayout layout;
 
     public fightPanel(){
+        layout = new GridLayout(0, 2, 0, 0);
+        setLayout(layout);
 
         player = new JLabel();
         player.setIcon(new javax.swing.ImageIcon("lib/GarenGreenPlayer.gif"));
+
+        computer = new JLabel();
+        computer.setIcon(new javax.swing.ImageIcon("lib/GarenGreenComputer.gif"));
         add(player);
+        add(computer);
     }
 
     public static void main(String[] args){
