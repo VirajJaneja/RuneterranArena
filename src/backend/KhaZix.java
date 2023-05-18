cbf0db765d4aaa8ad117ba16630945e0f8a255cf
Lethargic Logistics <61340443+LogisticsAndLaziness@users.noreply.github.com>
2023-05-18 07:39:45 -0400

    push
    !!

--- src/backend/KhaZix.java
+++ src/backend/KhaZix.java
@@ -7,8 +7,8 @@ import javax.swing.ImageIcon;
 
 public class KhaZix extends CharacterWrapperSq{
     private static String region = "Void";
-    private static String type = "Fighter";
-    private Ability a = new Ability("Unseen Threat", 30, 0.4, 0.25, false, false, false);
+    private static String type = "Mage";
+    private Ability a = new Ability("Unseen Threat", 40, 0.2, 0.25, false, false, false);
     private Ability b = new Ability("Void Spike", 45, 0.2, 0.1, false, false, false);
     private Ability c = new Ability("Taste their Fear", 45, 0.5, 0.1, false, false, true);
     private Ability d = new Ability("Auto Attack", 40, 0.2, 0.1, false, false, false);
--- src/backend/Taric.java
+++ src/backend/Taric.java
@@ -9,7 +9,7 @@ public class Taric extends CharacterWrapperSq{
     private static String type = "Fighter";
     private Ability a = new Ability("Bastion", 30, 0.4, 0.25, false, false, false);
     private Ability b = new Ability("Starlight's touch", 45, 0.2, 0.1, true, false, true);
-    private Ability c = new Ability("Cosmic Radiance", 45, 0.2, 0.1, false, false, true);
+    private Ability c = new Ability("Cosmic Radiance", 50, 0.7, 0.01, false, false, true);
     private Ability d = new Ability("Auto Attack", 40, 0.2, 0.1, false, false, false);
     private ImageIcon characterIcon = new ImageIcon("lib/icons/abilities/taric icon.png");
     private ImageIcon basickATKIcon = new ImageIcon("lib/icons/abilities/taric basicATK.png");
@@ -23,7 +23,7 @@ public class Taric extends CharacterWrapperSq{
     private ImageIcon spawnAnim = new ImageIcon("lib/animations/Taric/TaricIdle.gif");
     
     public Taric(int diff){
-        super(type, region, 80, diff);
+        super(type, region, 130, diff);
         // System.out.println(type + "," + region);
         setAbilities(a, b, c, d);
     }
